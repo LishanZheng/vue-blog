5087586fbf1c3735b29a89ba7a27a228b237a539
郑力煽 <602868253@qq.com>
2021-01-06 18:26:02 +0800

    修改设置

--- README.md
+++ README.md
@@ -16,5 +16,7 @@ npm run build
 
 # build for production and view the bundle analyzer report
 npm run build --report
+
+
 ```
 
--- src/main.js
+++ src/main.js
@@ -15,16 +15,9 @@ Vue.use(mavonEditor)
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 Vue.prototype.$qs = qs
-// axios.defaults.baseURL = 'http://127.0.0.1:8080'
-axios.defaults.baseURL = 'http://121.196.174.189:8080'
+axios.defaults.baseURL = 'http://127.0.0.1:8080'
+// axios.defaults.baseURL = 'http://121.196.174.189:8080'
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
-// axios.defaults.transformRequest = [
-//   function (data) {
-//     if(data.__proto__ === FormData.prototype)
-//       return data
-//     else
-//       return qs.stringify(data)
-// }]
 
 /* eslint-disable no-new */
 new Vue({
